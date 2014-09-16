443ea277d9c404abee637e2396f0c484963c3d66
Stephen Halter <halter73@gmail.com>
2014-09-15 18:57:23 -0700

    WIP

---
+++ src/Microsoft.AspNet.SignalR.Client/Infrastructure/TaskQueueMonitor.cs
@@ -0,0 +1,101 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.md in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Infrastructure;
+
+#if NETFX_CORE
+using Windows.System.Threading;
+#endif
+
+namespace Microsoft.AspNet.SignalR.Client.Infrastructure
+{
+    internal sealed class TaskQueueMonitor : ITaskMonitor, IDisposable
+    {
+#if !NETFX_CORE
+        private Timer _timer;
+#else
+        private ThreadPoolTimer _timer;
+#endif
+
+        private readonly IConnection _connection;
+        private readonly TimeSpan _deadlockErrorTimeout;
+ 
+        private readonly object _lockObj = new object();
+        private bool _isRunning;
+        private uint _taskId;
+        private uint _lastTaskId;
+
+        public TaskQueueMonitor(IConnection connection, TimeSpan deadlockErrorTimeout)
+        {
+            _connection = connection;
+            _deadlockErrorTimeout = deadlockErrorTimeout;
+
+#if !NETFX_CORE
+            _timer = new Timer(_ => Beat(), state: null, dueTime: deadlockErrorTimeout, period: deadlockErrorTimeout);
+#else
+            _timer = ThreadPoolTimer.CreatePeriodicTimer(_ => Beat(), period: deadlockErrorTimeout);
+#endif
+        }
+
+        public void TaskStarted()
+        {
+            lock (_lockObj)
+            {
+                Debug.Assert(!_isRunning);
+
+                _isRunning = true;
+                _taskId++;
+            }
+        }
+
+        public void TaskCompleted()
+        {
+            lock (_lockObj)
+            {
+                Debug.Assert(!_isRunning);
+
+                _isRunning = false;
+            }
+        }
+
+        // This is only able to detect deadlocks because Connection enqueues callbacks using
+        // Task.Factory.StartNew. Otherwise _queue.ExecutingTask would stay null during the deadlock.
+        internal void Beat()
+        {
+            lock (_lockObj)
+            {
+                if (_isRunning && _taskId == _lastTaskId)
+                {
+                    var errorMessage = String.Format(Resources.Error_PossibleDeadlockDetected,
+                                                     _deadlockErrorTimeout.TotalSeconds);
+                    _connection.OnError(new SlowCallbackException(errorMessage));
+                }
+
+                _lastTaskId = _taskId;
+            }
+        }
+
+        /// <summary>
+        /// Dispose off the timer
+        /// </summary>
+        public void Dispose()
+        {
+                if (_timer != null)
+                {
+#if !NETFX_CORE
+                    _timer.Dispose();
+                    _timer = null;
+#else
+                    _timer.Cancel();
+                    _timer = null;
+#endif
+                }
+        }
+    }
+}
---
+++ tests/Microsoft.AspNet.SignalR.Client.Tests/Client/Infrastructure/TaskQueueMonitorFacts.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using Moq;
+using Xunit;
+
+namespace Microsoft.AspNet.SignalR.Client.Infrastructure
+{
+    public class TaskQueueMonitorFacts
+    {
+        [Fact]
+        public void ErrorsAreTriggeredForLongRunningTasks()
+        {
+            var connection = new Mock<IConnection>();
+            var monitor = new TaskQueueMonitor(connection.Object, Timeout.InfiniteTimeSpan);
+
+            monitor.TaskStarted();
+
+            monitor.Beat();
+            monitor.Beat();
+
+            connection.Verify(c => c.OnError(It.IsAny<Exception>()), Times.Once());
+        }
+
+        [Fact]
+        public void ErrorsAreNotTriggeredMultipleTimesForTheSameTask()
+        {
+            var connection = new Mock<IConnection>();
+            var monitor = new TaskQueueMonitor(connection.Object, Timeout.InfiniteTimeSpan);
+
+
+            monitor.TaskStarted();
+
+            monitor.Beat();
+            monitor.Beat();
+            monitor.Beat();
+
+            connection.Verify(c => c.OnError(It.IsAny<Exception>()), Times.Once());
+        }
+
+        [Fact]
+        public void NoErrorsAreTriggeredBeforeATaskStarts()
+        {
+            var connection = new Mock<IConnection>();
+            var monitor = new TaskQueueMonitor(connection.Object, Timeout.InfiniteTimeSpan);
+
+            monitor.Beat();
+            monitor.Beat();
+
+            connection.Verify(c => c.OnError(It.IsAny<Exception>()), Times.Never());
+        }
+    }
+}
